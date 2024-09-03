b33b341fc8f348c2358eaef727985f05c2ab1c70
Thiago Hardt <thiagohrh@outlook.com>
2024-09-03 17:14:43 -0300

    Duplicate itens now are accounted for.
    Adds a function that tests if there's already a slot filled with that item type. Only adds to the item amount on the same slot.

--- Inventory-prototype/Assets/Scripts/ItemSO/BaseSOScripts/SOItemInventory.cs
+++ Inventory-prototype/Assets/Scripts/ItemSO/BaseSOScripts/SOItemInventory.cs
@@ -9,7 +9,7 @@ public class SOItemInventory : ScriptableObject
     private List<ItemSlotData> _itemSlotDataList = new List<ItemSlotData>();
     public List<ItemSlotData> ItemSlotDataList => _itemSlotDataList; // For simple info fetching.
     [Serializable]
-    public struct ItemSlotData
+    public class ItemSlotData
     {
         public ItemIDs ItemId;
         public int ItemAmount;
@@ -19,12 +19,34 @@ public class SOItemInventory : ScriptableObject
     {
         if (newItemAmount <= 0 || newItemId == ItemIDs.DEFAULT)
             return;
+        int itemIndex;
+        
+        if (IsItemAlreadyOnInventory(newItemId, out itemIndex))
+        {
+            _itemSlotDataList[itemIndex].ItemAmount += newItemAmount;
+            return;
+        }
         
         ItemSlotData newItemSlot = new ItemSlotData();
         newItemSlot.ItemId = newItemId;
         newItemSlot.ItemAmount = newItemAmount;
         
         _itemSlotDataList.Add(newItemSlot);
+    }
+
+    private bool IsItemAlreadyOnInventory(ItemIDs newItemId, out int itemPlacementIndex)
+    {
+        itemPlacementIndex = 0;
+        for (int i = 0; i < _itemSlotDataList.Count; i++)
+        {
+            if (_itemSlotDataList[i].ItemId == newItemId)
+            {
+                itemPlacementIndex = i;
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public void RemoveItemFromInventorySlot(ItemIDs newItemId, int newItemAmount) // If I use an item...
